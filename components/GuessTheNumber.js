da395c00eb251a1a60538423e84cc6665940166d
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-28 19:59:57 +0800

    added guessing logic

--- components/GuessTheNumber.js
+++ components/GuessTheNumber.js
@@ -11,8 +11,22 @@ export default function GuessTheNumber() {
   const [result, setResult] = useState(Math.floor(Math.random() * 100));
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(100);
-  const [guess, setGuess] = useState();
-  const handlePress = () => {};
+  const [guess, setGuess] = useState(0);
+  const handlePress = () => {
+    if (guess > max || guess < min) {
+      console.log(typeof guess);
+      alert('Your guess is out of the range! Enter another number to submit.');
+      return;
+    }
+
+    if (guess === result) {
+      alert('You win!');
+    } else {
+      guess < result ? setMin(guess) : setMax(guess);
+    }
+    setGuess();
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.gameHeader}>Game Header</Text>
@@ -21,11 +35,11 @@ export default function GuessTheNumber() {
       </View>
       <Text>Current Min: {min}</Text>
       <Text>Current Max: {max}</Text>
-      <Text>Your guess: {guess ? guess : '-'}</Text>
       <TextInput
         keyboardType='numeric'
         placeholder='Type here...'
         style={styles.textInput}
+        value={guess}
         onChangeText={(guess) => setGuess(guess)}
       />
       <TouchableOpacity style={styles.button} onPress={handlePress}>
