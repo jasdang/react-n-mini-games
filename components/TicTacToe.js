1489791a83a5adba2c6d085c89b2ea4f14d18eb3
Jasmine Dang <ngandth.1992@gmail.com>
2020-11-08 15:05:59 +0800

    decide winner

--- components/TicTacToe.js
+++ components/TicTacToe.js
@@ -18,6 +18,7 @@ export default function TicTacToe() {
       <Text style={styles.gameHeader} onPress={() => handleClick(1)}>
         Next Player is {xIsNext ? 'X' : 'O'}
       </Text>
+      <Text style={styles.gameHeader}>Winner is {calculateWinner(cells)}</Text>
       <Board
         cells={cells}
         xIsNext={xIsNext}
@@ -64,6 +65,26 @@ function Cell(props) {
       </View>
     </TouchableOpacity>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 const styles = StyleSheet.create({
